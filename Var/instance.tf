e25336fdd727ec1546733ddbe9efc3855ed35f90
chaima-elm <chaimaelmerrak@gmail.com>
2023-10-13 00:34:32 +0100

    mod

--- Var/instance.tf
+++ Var/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "shamy-inst" {
+resource "aws_instance" "shamy-inst " {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
